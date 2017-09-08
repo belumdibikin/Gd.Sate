d67a30ef2d1394c893361411a3c259e05138389d
belumdibikin <bhonzchelz92@gmail.com>
2017-09-09 03:15:32 +0700

    config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$_SERVER['HTTP_HOST'] = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : "";
+$_SERVER['SCRIPT_NAME'] = isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : "";
+$_SERVER['REMOTE_ADDR'] = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : "";
+$protocol = (!empty($_SERVER['HTTPS'])) ? 'https://' : 'http://';
+$root = $protocol.$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "$root";
+// $config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
