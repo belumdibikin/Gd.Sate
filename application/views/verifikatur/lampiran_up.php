1d057b4d8d20d6d65a4bca1cc17e3c858dcea73b
rizqiaulia <rizqiauliapasca@gmail.com>
2017-09-09 03:11:47 +0700

    lampiran up
    buat ngegantiin coding lampiran di terima berkas up

---
+++ application/views/verifikatur/lampiran_up.php
@@ -0,0 +1,99 @@
+<div class="panel">
+										<h5> Kelengkapan Lampiran</h5>
+										<div class="heading">
+											<span class="title">Lampiran</span>
+										</div>
+										<div class="content">
+											<div class="row cells6">
+												<div class="cell colspan3">
+													Anggaran Kas
+
+												</div>
+												<div class="cell">
+													:
+												</div>
+												<div class="cell colspan2">
+													<label class="switch">
+														<input type="checkbox">
+														<span class="check"></span>
+													</label>
+												</div>
+											</div>
+											<div class="row cells6">
+												<div class="cell colspan3">
+													Check list SPP (tanda tangan PPK-SKPD)
+
+												</div>
+												<div class="cell">
+													:
+												</div>
+												<div class="cell colspan2">
+													<label class="switch">
+														<input type="checkbox">
+														<span class="check"></span>
+													</label>
+												</div>
+											</div>
+											<div class="row cells6">
+												<div class="cell colspan3">
+													DPA
+
+												</div>
+												<div class="cell">
+													:
+												</div>
+												<div class="cell colspan2">
+													<label class="switch">
+														<input type="checkbox">
+														<span class="check"></span>
+													</label>
+												</div>
+											</div>
+											<div class="row cells6">
+												<div class="cell colspan3">
+													Nota Dinas Permohonan Penerbitan SPM
+
+												</div>
+												<div class="cell">
+													:
+												</div>
+												<div class="cell colspan2">
+													<label class="switch">
+														<input type="checkbox">
+														<span class="check"></span>
+													</label>
+												</div>
+											</div>
+											<div class="row cells6">
+												<div class="cell colspan3">
+													Rencana Pencairan
+
+												</div>
+												<div class="cell">
+													:
+												</div>
+												<div class="cell colspan2">
+													<label class="switch">
+														<input type="checkbox">
+														<span class="check"></span>
+													</label>
+												</div>
+											</div>
+											<div class="row cells6">
+												<div class="cell colspan3">
+													SPD dari BUD
+
+												</div>
+												<div class="cell">
+													:
+												</div>
+												<div class="cell colspan2">
+													<label class="switch">
+														<input type="checkbox">
+														<span class="check"></span>
+													</label>
+												</div>
+											</div>
+										</div>
+
+									</div>
